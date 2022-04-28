2fe7d29ac96ff31499b3702159a22f55c8259b03
wrynnsun <wrynnsun@tencent.com>
2022-04-28 16:28:43 +0800

    静态资源分割

---
+++ src/components/TestComp/TestTest.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3>展示一些静态资源</h3>
+  <img src="./win.apng.png" alt="胜利" />
+</template>
+
+<script lang="ts" setup>
+//
+</script>
+
+<style lang="less" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+const ASSETS_DIR = 'res';
+const OUT_DIR = 'dist';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 8888,
+  },
+
+  // 编译配置
+  build: {
+    outDir: OUT_DIR,
+    assetsDir: ASSETS_DIR,
+    rollupOptions: {
+      output: {
+        chunkFileNames: `${ASSETS_DIR}/js/[name]-[hash].js`,
+        entryFileNames: `${ASSETS_DIR}/js/[name]-[hash].js`,
+
+        assetFileNames: ({ name }) => {
+          const namePattern = '[name]-[hash][extname]';
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            return `${ASSETS_DIR}/images/${namePattern}`;
+          }
+
+          if (/\.css$/.test(name ?? '')) {
+            return `${ASSETS_DIR}/css/${namePattern}`;
+          }
+
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return `${ASSETS_DIR}/${namePattern}`;
+        },
+      },
+    },
   },
 });
