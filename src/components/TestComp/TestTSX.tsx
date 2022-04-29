84a4902e56e17ddc051617a050ee58d06e99e950
iskl <10909568@qq.com>
2022-04-30 01:18:17 +0800

    config

--- src/components/TestComp/TestTSX.tsx
+++ src/components/TestComp/TestTSX.tsx
@@ -1,5 +1,5 @@
 import { defineComponent } from 'vue';
-import testTSXStyle from './TestTSX.module.less';
+import testTSXStyle from '@/components/TestComp/TestTSX.module.less';
 
 export default defineComponent({
   render: () => (
