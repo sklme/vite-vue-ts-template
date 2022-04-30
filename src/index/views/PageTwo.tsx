230a6c842322c603da29f9d401aae720b8f09694
iskl <10909568@qq.com>
2022-05-01 01:03:33 +0800

    global

--- src/index/views/PageTwo.tsx
+++ src/index/views/PageTwo.tsx
@@ -5,6 +5,7 @@ function render() {
     <div class="page-2">
       <div class={style.title}>页面2</div>
       <div class={style.pageDesc}>这个页面2，使用tsx编写</div>
+      <div class="the-red">红色红色</div>
     </div>
   );
 }
