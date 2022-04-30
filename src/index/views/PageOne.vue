1717a776b29506b565f84a2faaaac145f48c1cda
iskl <10909568@qq.com>
2022-05-01 01:37:25 +0800

    example

--- src/index/views/PageOne.vue
+++ src/index/views/PageOne.vue
@@ -5,16 +5,17 @@
     <p>第{{ x }}次点击</p>
     <button @click="clickX">点击我</button>
   </div>
-  <div>
+  <div class="vuex">
     从vuex渲染
     <div>{{ testText }}</div>
     <button @click="modifyTest">trigger commit</button>
+    <div>{{ test123 }}</div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useStore } from 'vuex';
-import { computed, ref } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 const store = useStore();
 
 const x = ref(0);
@@ -22,6 +23,24 @@ function clickX() {
   x.value++;
 }
 
-const testText = computed(() => store.state.test);
+const testText = computed(() => `xxxx ${store.state.test} xxxx`);
 const modifyTest = () => store.commit('modifyTest');
 </script>
+
+<script lang="ts">
+import _store from '@/store/index';
+
+export default defineComponent({
+  computed: {
+    test123() {
+      return `123${_store.state.test}123`;
+    },
+  },
+});
+</script>
+
+<style lang="less" scoped>
+.vuex {
+  background-color: #ddd;
+}
+</style>
